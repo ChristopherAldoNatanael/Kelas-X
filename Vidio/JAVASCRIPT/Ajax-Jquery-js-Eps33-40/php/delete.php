82cb0d475c67cb85773da73059418efcef7a24d6
Aldoo <christophernatanael123@gmail.com>
2024-10-07 07:53:45 +0700

    Add files via upload

---
+++ Vidio/JAVASCRIPT/Ajax-Jquery-js-Eps33-40/php/delete.php
@@ -0,0 +1,25 @@
+<?php 
+
+    require_once "koneksi.php";
+
+    $data = stripslashes(file_get_contents("php://input"));
+    $idpelanggan = json_decode($data, true);
+    
+    $idpelanggan = $idpelanggan['idpelanggan'];
+    
+    
+    
+    if (!empty($idpelanggan)) {
+        
+        $sql = "DELETE FROM tblpelanggan WHERE idpelanggan = $idpelanggan";
+
+    if ($result = mysqli_query($con, $sql)) { 
+        echo "Data Sudah Di Hapus!";
+    } else {
+        echo "Data Belum Di pilih"; 
+    }
+
+} else {
+    echo "Data Kosong";
+}
+?>
